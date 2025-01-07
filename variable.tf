1d2517c77ab1e0a8154f2440801287c9bbd25901
rehankhan008 <rehanrocks555@outlook.com>
2025-01-07 17:07:56 +0530

    second

--- variable.tf
+++ variable.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "terr-backend"
+    key = "terraform.tfstate"
+    region = "ap-south-1"  
+  }
 }
 
 # Configure the AWS Provider
